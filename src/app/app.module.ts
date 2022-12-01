70b8b937248817f90bea16d3841bc705351c1c8a
Sean Misra <seanmisra@Seans-MacBook-Pro-2.local>
2022-12-01 10:13:45 -0500

    Component architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderBarComponent } from './header-bar/header-bar.component';
+import { FormWrapperComponent } from './form-wrapper/form-wrapper.component';
+import { UserInputsComponent } from './form-wrapper/user-inputs/user-inputs.component';
+import { MovieRecommendationComponent } from './form-wrapper/movie-recommendation/movie-recommendation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderBarComponent,
+    FormWrapperComponent,
+    UserInputsComponent,
+    MovieRecommendationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/form-wrapper/movie-recommendation/movie-recommendation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'movie-recommendation',
+  templateUrl: './movie-recommendation.component.html',
+  styleUrls: ['./movie-recommendation.component.scss']
+})
+export class MovieRecommendationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
