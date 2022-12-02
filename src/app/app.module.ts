22db1f04e52861568826180a8920e081d429562b
Sean Misra <seanmisra30@gmail.com>
2022-12-02 16:11:14 -0500

    Fix compilation error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,11 @@ import { AppComponent } from './app.component';
 import { HeaderBarComponent } from './header-bar/header-bar.component';
 import { FormWrapperComponent } from './form-wrapper/form-wrapper.component';
 import { UserInputsComponent } from './form-wrapper/user-inputs/user-inputs.component';
-import { MovieRecommendationComponent } from './form-wrapper/movie-recommendation/movie-recommendation.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule, MatAutocompleteModule, MatCardModule  } from '@angular/material';
 import { MatButtonModule } from '@angular/material/button';
 import { HttpClientModule } from '@angular/common/http';
+import { MovieRecommendationComponent } from './movie-recommendation/movie-recommendation.component';
 
 @NgModule({
   declarations: [
