d43545b1127519cb59abf8c1b332170a6cef3457
Sean Misra <seanmisra30@gmail.com>
2022-12-05 13:26:17 -0500

    Use NodeJS for data APIs

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.1.3.
 
-It provides movie recommendations based on 1-3 inputed favorite movies. The recommendations are based on keyword matching. 
+It provides movie recommendations based on 1-3 inputed favorite movies. The recommendations are based on keyword matching. The recommendation node-js APIs are in [movie-picker-node-apis](https://github.com/seanmisra/movie-picker-node-apis).
 
 Images in the recommendations are fetched from the [OMDb API](https://www.omdbapi.com/).
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -19,74 +19,26 @@ export class MovieService {
   MOVIE_DETAILS_ID = config.MOVIE_ID;
   MOVIE_DETAILS_KEY = config.MOVIE_KEY;
 
+  MOVIE_API_BASE_URL = config.MOVIE_API_BASE_URL ? config.MOVIE_API_BASE_URL : 'http://localhost';
+  MOVIE_API_PORT = config.MOVIE_API_PORT ? config.MOVIE_API_PORT : 8080;
+
   constructor(private http: HttpClient) { 
   }
 
   getAllMovies(): Observable<any> {
-    return of(allMovieData.sort((a, b) => (a.movieName > b.movieName) ? 1 : -1));
-  }
-  
-  private lookupMovie(lookupMovieName: string): MovieData {
-    return this.allMovieDataLocal.find(movie => movie.movieName.toLowerCase() === lookupMovieName.toLowerCase()); 
-
+    let url = this.MOVIE_API_BASE_URL + ':' + this.MOVIE_API_PORT + '/allMovies';
+    return this.http.get(url);
   }
 
-
-  getMovieRecommendation(userData: UserData): Observable<Recommendation> {
-    this.allMovieDataLocal = allMovieData;
-
-
-    let movieOne: MovieData
-    let movieTwo: MovieData;
-    let movieThree: MovieData;
-    let relevantKeywords: any[] = [];
-    
-    movieOne = this.lookupMovie(userData.movieOne);
-    if (movieOne) {
-      relevantKeywords.push(...movieOne.keywords);
-      this.allMovieDataLocal = this.allMovieDataLocal.filter(movie => movie.movieName !== movieOne.movieName);      
-    }
-    
-    
-    if (userData.movieTwo) {
-      movieTwo = this.lookupMovie(userData.movieTwo);
-      if (movieTwo) {
-        relevantKeywords.push(...movieTwo.keywords);
-        this.allMovieDataLocal = this.allMovieDataLocal.filter(movie => movie.movieName !== movieTwo.movieName);      
-      }
-    }
-    if (userData.movieThree) {
-      movieThree = this.lookupMovie(userData.movieThree);
-      if (movieThree) {
-        relevantKeywords.push(...movieThree.keywords);
-        this.allMovieDataLocal = this.allMovieDataLocal.filter(movie => movie.movieName !== movieThree.movieName);      
-      }
+  getMovieRecommendation(userData: UserData): Observable<any> {
+    let url = this.MOVIE_API_BASE_URL + ':' + this.MOVIE_API_PORT + '/recommendation';
+    let body = {
+      movieOneInput: userData.movieOne,
+      movieTwoInput: userData.movieTwo,
+      movieThreeInput: userData.movieThree
     }
 
-    // unique array of applicable keywords 
-    relevantKeywords = [...new Set(relevantKeywords)];
-    console.log("relevantKeywords");
-    console.log(relevantKeywords);
-
-    const movieScores: { [id: string] : number; } = {};
-    
-    this.allMovieDataLocal.forEach((movieObj: MovieData) => {
-      const sharedKeywords = movieObj.keywords.filter((kw: string) => relevantKeywords.includes(kw)); 
-      const score = sharedKeywords.length;
-      movieScores[movieObj.movieName] = score; 
-    });    
-    console.log("movieScores");
-    console.log(movieScores);
-
-    const recommendation = Object.keys(movieScores).reduce((a, b) => movieScores[a] > movieScores[b] ? a : b);
-    const recommendationObj: Recommendation = this.allMovieDataLocal.find(movie => movie.movieName.toLowerCase() === recommendation.toLowerCase()); 
-    recommendationObj.hasData = true;
-    console.log("recommendationObj");
-    console.log(recommendationObj);
-
-    return of(
-      recommendationObj  
-    );
+    return this.http.post(url, body);
   }
 
   getEnrichedMovieData(movieTitle: string): Observable<any> {
