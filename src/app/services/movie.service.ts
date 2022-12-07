af5ab6f4aaff2c353f8ca22d2c3fbb1426dac218
Sean Misra <seanmisra30@gmail.com>
2022-12-07 17:34:33 -0500

    Service tweaks for deployment to AWS

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -18,20 +18,27 @@ export class MovieService {
   MOVIE_DETAILS_BASE_URL = 'http://www.omdbapi.com';
   MOVIE_DETAILS_ID = config.MOVIE_ID;
   MOVIE_DETAILS_KEY = config.MOVIE_KEY;
-
-  MOVIE_API_BASE_URL = config.MOVIE_API_BASE_URL ? config.MOVIE_API_BASE_URL : 'http://localhost';
-  MOVIE_API_PORT = config.MOVIE_API_PORT ? config.MOVIE_API_PORT : 8080;
+  PROD_ENV = config.PROD_ENV === 'true' ? true : false;
 
   constructor(private http: HttpClient) { 
   }
 
   getAllMovies(): Observable<any> {
-    let url = this.MOVIE_API_BASE_URL + ':' + this.MOVIE_API_PORT + '/allMovies';
+    let url = '';
+    if (!this.PROD_ENV) {
+      url = 'http://localhost:8080';
+    } 
+    url += '/api/allMovies';
     return this.http.get(url);
   }
 
   getMovieRecommendation(userData: UserData): Observable<any> {
-    let url = this.MOVIE_API_BASE_URL + ':' + this.MOVIE_API_PORT + '/recommendation';
+    let url = '';
+    if (!this.PROD_ENV) {
+      url = 'http://localhost:8080';
+    } 
+    url += '/api/recommendation';
+
     let body = {
       movieOneInput: userData.movieOne,
       movieTwoInput: userData.movieTwo,
